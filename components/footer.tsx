5975c27daea1cfd51cce7c175ff964012fe74de8
husinzz <its.husin@gmail.com>
2021-12-05 00:24:17 +0700

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,17 +25,17 @@ export default function Footer() {
       </div>
       <div className="block md:hidden">
         <div className="text-center">
-          <a href="tel:+6282219738846">
-            <i className="bg-white rounded-full text-black text-3xl p-3 mx-5 fas fa-phone"></i>
+          <a href="tel:+62 822 1973 8846">
+            <i className="bg-white rounded-full text-black text-3xl p-3 mx-2 fas fa-phone"></i>
           </a>
-          <a href="https://wa.me/6282219738846">
-            <i className="bg-white rounded-full text-black text-3xl p-3 mx-5 fab fa-whatsapp"></i>
+          <a target="_blank" rel="noreferrer" href="https://wa.me/6282219738846">
+            <i className="bg-white rounded-full text-black text-3xl p-3 mx-2 fab fa-whatsapp"></i>
           </a>
-          <a href="https://linkedin/in/husinzz">
-            <i className="bg-white rounded-full text-black text-3xl p-3 mx-5 fab fa-linkedin"></i>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/husinzz/">
+            <i className="bg-white rounded-full text-black text-3xl p-3 mx-2 fab fa-linkedin"></i>
           </a>
-          <a href="https://www.youtube.com/channel/UC2kAQEKEYzDF9CuP1qr5AUw">
-            <i className="bg-white rounded-full text-black text-3xl p-3 mx-5 fab fa-youtube"></i>
+          <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UC2kAQEKEYzDF9CuP1qr5AUw">
+            <i className="bg-white rounded-full text-black text-3xl p-3 mx-2 fab fa-youtube"></i>
           </a>
         </div>
       </div>
